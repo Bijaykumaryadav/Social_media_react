f05c03b19f2de3df49677f38bf6dbac5a6a01bf9
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-06-14 16:41:36 +0530

    woriking on the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./routes/App.jsx";
 import CreatePost from "./components/CreatePost.jsx";
+import PostList from "./components/PostList.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/create-post",
-    element: <CreatePost />,
+    children: [
+      {
+        path: "/",
+        element: <PostList />,
+      },
+      {
+        path: "/create-post",
+        element: <CreatePost />,
+      },
+    ],
   },
 ]);
 
